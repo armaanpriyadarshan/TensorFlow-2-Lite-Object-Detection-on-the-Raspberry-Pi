baf853f9822a4c24d40135fbfd32c1f7b7db78d3
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-11-15 12:57:41 -0500

    Add files via upload

---
+++ TFLite-image-od.py
@@ -0,0 +1,132 @@
+import tflite_runtime.interpreter as tflite
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import time
+from threading import Thread
+import importlib.util
+
+# Define VideoStream class to handle streaming of video from webcam in separate processing thread
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+        
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help='Provide the path to the TFLite file, default is models/model.tflite',
+                    default='models/model.tflite')
+parser.add_argument('--labels', help='Provide the path to the Labels, default is models/labels.txt',
+                    default='models/labels.txt')
+parser.add_argument('--image', help='Name of the single image to perform detection on',
+                    default='test.png')
+parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
+                    default=0.5)
+parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+                    default='1280x720')
+                    
+args = parser.parse_args()
+
+# PROVIDE PATH TO MODEL DIRECTORY
+PATH_TO_MODEL_DIR = args.model
+
+# PROVIDE PATH TO LABEL MAP
+PATH_TO_LABELS = args.labels
+
+# PROVIDE PATH TO IMAGE
+IMAGE_PATH = args.image
+
+# PROVIDE THE MINIMUM CONFIDENCE THRESHOLD
+MIN_CONF_THRESH = float(args.threshold)
+
+resW, resH = args.resolution.split('x')
+imW, imH = int(resW), int(resH)
+import time
+print('Loading model...', end='')
+start_time = time.time()
+
+# LOAD TFLITE MODEL
+interpreter = tflite.Interpreter(model_path=PATH_TO_MODEL_DIR)
+# LOAD LABELS
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
+
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+# Initialize frame rate calculation
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+
+# Start timer (for calculating frame rate)
+current_count=0
+t1 = cv2.getTickCount()
+print('Running inference for {}... '.format(IMAGE_PATH), end='')
+
+# Acquire frame and resize to expected shape [1xHxWx3]
+image = cv2.imread(IMAGE_PATH)
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_resized = cv2.resize(image_rgb, (width, height))
+input_data = np.expand_dims(image_resized, axis=0)
+
+# Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+if floating_model:
+    input_data = (np.float32(input_data) - input_mean) / input_std
+
+# Perform the actual detection by running the model with the image as input
+interpreter.set_tensor(input_details[0]['index'],input_data)
+interpreter.invoke()
+
+# Retrieve detection results
+boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
+classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
+scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
+#num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+
+# Loop over all detections and draw detection box if confidence is above minimum threshold
+for i in range(len(scores)):
+    if ((scores[i] > MIN_CONF_THRESH) and (scores[i] <= 1.0)):
+
+        # Get bounding box coordinates and draw box
+        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+        ymin = int(max(1,(boxes[i][0] * imH)))
+        xmin = int(max(1,(boxes[i][1] * imW)))
+        ymax = int(min(imH,(boxes[i][2] * imH)))
+        xmax = int(min(imW,(boxes[i][3] * imW)))
+        
+        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+        # Draw label
+        object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+        label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+        label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+        cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+        cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+        current_count+=1
+# Calculate framerate
+t2 = cv2.getTickCount()
+time1 = (t2-t1)/freq
+frame_rate_calc= 1/time1
+# Draw framerate in corner of frame
+cv2.putText(image,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+cv2.putText (image,'Total Detection Count : ' + str(current_count),(30,100),cv2.FONT_HERSHEY_SIMPLEX,1,(98,189,184),2,cv2.LINE_AA)
+# All the results have been drawn on the frame, so it's time to display it.
+cv2.imshow('Object detector', image)
+
+
+# Press any key to quit
+cv2.waitKey(0)
+
+# Clean up
+cv2.destroyAllWindows()
