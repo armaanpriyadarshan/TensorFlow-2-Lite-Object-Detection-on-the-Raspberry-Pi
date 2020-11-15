94f2ffe4be73e21144a8e4d56b5198f3f4eb1374
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-11-15 12:42:11 -0500

    Create install-prerequisites.sh

---
+++ install-prerequisites.sh
@@ -0,0 +1,13 @@
+# Installs Prerequisites for OpenCV
+
+sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+
+# Installs OpenCV pip package
+
+pip install opencv-python==4.1.0.25
+
+# Install the tflite_runtime
+pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
