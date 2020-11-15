0029a66191c065f0d287991a9f37493a4f433612
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-11-15 16:28:30 -0500

    Update install-prerequisites.sh

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -11,3 +11,4 @@ pip install opencv-python==4.1.0.25
 
 # Install the tflite_runtime
 pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+echo Prerequisites Installed Successfully
