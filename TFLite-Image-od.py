6fd7a069ab6adefceff36595ede934e72842dadc
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-11-22 16:19:19 +0000

    Update TFLite-Image-od.py

--- TFLite-Image-od.py
+++ TFLite-Image-od.py
@@ -23,8 +23,6 @@ parser.add_argument('--image', help='Name of the single image to perform detecti
                     default='test.png')
 parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
                     default=0.5)
-parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-                    default='1280x720')
                     
 args = parser.parse_args()                              
 
